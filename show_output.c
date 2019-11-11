d6d19f49c360a28a87588ecb847d4489ae2d0c78
Niko Gontjarow <ngontjar@c3r3p1.hive.fi>
2019-11-11 15:27:22 +0200

    added some basic API structure to the program

--- show_output.c
+++ show_output.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   show_output.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/08 15:09:27 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/11/11 12:15:44 by ngontjar         ###   ########.fr       */
+/*   Created: 2019/11/11 12:12:45 by ngontjar          #+#    #+#             */
+/*   Updated: 2019/11/11 12:31:04 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include "libft/libft.h"
 
-# include <fcntl.h>
-# include "get_next_line.h"
+void	show_output(char **square)
+{
+	int		row;
 
-char	***input_read(int fd);
-char	**smallest_square(char ***array2D);
-void	show_output(char **square);
-
-#endif
+	row = 0;
+	while (square[row] != NULL)
+	{
+		ft_putendl(square[row]);
+	}
+}
--- smallest_square.c
+++ smallest_square.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   smallest_square.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/08 15:09:27 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/11/11 12:15:44 by ngontjar         ###   ########.fr       */
+/*   Created: 2019/11/11 12:12:51 by ngontjar          #+#    #+#             */
+/*   Updated: 2019/11/11 15:20:33 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include "libft/libft.h"
 
-# include <fcntl.h>
-# include "get_next_line.h"
-
-char	***input_read(int fd);
-char	**smallest_square(char ***array2D);
-void	show_output(char **square);
-
-#endif
+char	**smallest_square(char ****array2D)
+{
+	// lord help me
+}
