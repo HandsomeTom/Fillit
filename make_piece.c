374ce2eb764439ee1cfa8aa708cd3eca6192a28e
ngontjar <54755292+Gontjarow@users.noreply.github.com>
2019-11-22 01:06:34 +0200

    added new_piece which allocates and initializes all members of t_piece

--- make_piece.c
+++ make_piece.c
@@ -6,36 +6,66 @@
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 13:40:42 by tmaarela          #+#    #+#             */
-/*   Updated: 2019/11/21 01:13:50 by ngontjar         ###   ########.fr       */
+/*   Updated: 2019/11/21 06:00:56 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void	set_dimensions(t_piece **piece)
+static void	set_dimensions(t_piece *piece)
 {
 	int		width = 0;
 	int		height = 0;
 
-
+	piece->width = 0;
+	piece->height = 0;
 	while (height < 4)
 	{
 		width = 0;
 		while (width < 4)
 		{
-			if ((**piece).coords[height][width])
+			if (piece->coords[height][width])
 			{
-				if (width > (**piece).width)
-					(**piece).width += 1;
-				if (height > (**piece).height)
-					(**piece).height += 1;
+				if (width > piece->width)
+					piece->width += 1;
+				if (height > piece->height)
+					piece->height += 1;
 			}
 			++width;
 		}
 		++height;
 	}
-	(**piece).width += 1;
-	(**piece).height += 1;
+	piece->width += 1;
+	piece->height += 1;
+}
+
+static t_piece	*new_piece()
+{
+	t_piece	*piece;
+	int		x;
+	int		y;
+
+	if ((piece = (t_piece *)malloc(sizeof(t_piece))))
+	{
+		piece->width = 0;
+		piece->height = 0;
+		piece->letter = 'A';
+		piece->next = NULL;
+		x = 0;
+		y = 0;
+		while (y < 4)
+		{
+			x = 0;
+			while (x < 4)
+			{
+				piece->coords[y][x] = 0;
+				++x;
+			}
+			++y;
+		}
+
+	}
+	return (piece);
 }
 
 t_piece		*make_piece(char *str, char letter)
@@ -45,7 +75,7 @@ t_piece		*make_piece(char *str, char letter)
 	int		x;
 	int		y;
 
-	piece = (t_piece *)malloc(sizeof(t_piece));
+	piece = new_piece();
 	len = ft_strlen(str);
 	x = 0;
 	y = 0;
@@ -54,19 +84,15 @@ t_piece		*make_piece(char *str, char letter)
 		while (x < 4 && ((y * 4) + x) < len)
 		{
 			if (str[(y * 4) + x] == '#')
-			{
 				piece->coords[y][x] = 1;
-			}
 			else
-			{
 				piece->coords[y][x] = 0;
-			}
 			++x;
 		}
 		x = 0;
 		++y;
 	}
 	piece->letter = letter;
-	set_dimensions(&piece);
+	set_dimensions(piece);
 	return (piece);
 }
